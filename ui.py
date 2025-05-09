34afa393f256d4522d05222fb572522b5b5b3a45
quarterback <contact@ronbronson.com>
2025-05-09 01:24:57 -0700

    Update ui.py

--- ui.py
+++ ui.py
@@ -1,8 +1,3 @@
-import os
-os.environ["STREAMLIT_SERVER_PORT"] = "8080"
-os.environ["STREAMLIT_SERVER_HEADLESS"] = "true"
-os.environ["STREAMLIT_SERVER_ADDRESS"] = "0.0.0.0"
-
 import streamlit as st
 
 st.title("Hello, Streamlit!")
