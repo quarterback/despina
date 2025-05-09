4e6b3f687652d935f0c80de8167d9d573d475faf
quarterback <contact@ronbronson.com>
2025-05-09 01:08:53 -0700

    Update ui.py

--- ui.py
+++ ui.py
@@ -1,6 +1,8 @@
 import os
 os.environ["STREAMLIT_SERVER_PORT"] = "8080"
 os.environ["STREAMLIT_SERVER_HEADLESS"] = "true"
+os.environ["STREAMLIT_SERVER_ADDRESS"] = "0.0.0.0"
+
 from dotenv import load_dotenv
 import streamlit as st
 
